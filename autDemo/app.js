888555bc5247f89f21c71703b63ef39155aa3365
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-04-09 13:10:49 +0300

    register - login - logout

--- autDemo/app.js
+++ autDemo/app.js
@@ -1,42 +1,98 @@
-var express                 = require("express"),
-    app                     = express(),
-    bodyParser              = require("body-parser"),
-    mongoose                = require("mongoose"),
-    User                    = require("./models/user"),
-    LocalStrategy           = require("passport-local"),
-    passport                = require("passport"),
-    passportLocalMongoose   = require("passport-local-mongoose"),
-    expressSession          = require("express-session");
-    
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    User = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passport = require("passport"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    expressSession = require("express-session");
+
 mongoose.connect("mongodb://localhost/auth_demo");
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(expressSession({
-    secret : "Bu Bir Session Express Uygulamasidir..",
-    resave : false,
-    saveUninitialized : false
+    secret: "Bu Bir Session Express Uygulamasidir..",
+    resave: false,
+    saveUninitialized: false
 }));
 
 app.use(passport.initialize()); // passwordu initalize ediyoruz..
 app.use(passport.session());    // passworddan session metodunu cagir
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate())); // giris yapma istegi satirindaki authenticate'nin calismasini sagliyor.. 
+passport.serializeUser(User.serializeUser()); // password'u encode et..
+passport.deserializeUser(User.deserializeUser()); // password'u decode et..
 
+// Routes
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/gizli", function(req, res){
+//==========================================================================//
+
+// KAYDOL Routes
+app.get("/kaydol", function(req, res){
+    res.render("kaydol");
+});
+
+// kayit istegi gonder..
+app.post("/kaydol", function(req, res){
+    User.register(new User({username : req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("home");
+        } else {
+            passport.authenticate("local")(req, res, function(){ // local : bu bir strateji.. yani passport-local'deki local
+                res.redirect("/gizli"); // gizli sayfasina yonlendir..
+            });
+        }
+    });
+});
+
+//==========================================================================//
+
+// GIRIS Routes
+app.get("/giris", function(req, res){
+    res.render("giris");
+});
+
+// Giris Yapma İsteği Gonder
+app.post("/giris", passport.authenticate("local", {
+    successRedirect: "/gizli",
+    failureRedirect: "/giris"
+}), function(req, res){
+    
+});
+
+//==========================================================================//
+
+// CIKIS Routes
+app.get("/cikis", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+//==========================================================================//
+
+function isLogggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        res.redirect("/giris");
+    }
+}
+
+// GIZLI Route
+app.get("/gizli", isLogggedIn ,function (req, res) {
     res.render("gizli");
 });
 
-
 //==========================================================================//
-var server = app.listen(3000, function(){
+var server = app.listen(3000, function () {
     console.log("Sunucu Portu : ", server.address().port);
 });
