5eb7961b00b0b242f78b61e3403d38efee4c855a
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-28 01:56:30 +0300

    Movie endpoint i icin post metodunun yazilmasi

--- movieAPI/helper/db.js
+++ movieAPI/helper/db.js
@@ -10,4 +10,6 @@ module.exports = () => {
     mongoose.connection.on('error', (err) => {
        console.log('MongoDB : Connection Error....', err);
     });
+
+    mongoose.Promise = global.Promise;
 };
--- movieAPI/routes/movie.js
+++ movieAPI/routes/movie.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({ status : 1});
+//Models
+const Movie = require('../models/Movie');
+
+router.post('/', (req, res, next) => {
+  const {title, imdbScore, category, country, year} = req.body;
+
+  const movie = new Movie({
+    title: title,
+    imdbScore: imdbScore,
+    category: category,
+    country: country,
+    year: year
+  });
+
+
+  // Veri Kaydetme 1.Yontem Start
+/*
+  movie.save((err, data) => {
+
+    if(err)
+      res.json(err);
+    else
+      res.json(data);
+
+  });
+*/
+  // Veri Kaydetme 1.Yontem End
+
+
+  // Veri Kaydetme 2.Yontem Start
+  const promise = movie.save();
+  promise.then((data) => {
+    res.json({data});
+  }).catch((err) => {
+    res.json({err});
+  });
+  // Veri Kaydetme 2.Yontem End
+
 });
 
 module.exports = router;
