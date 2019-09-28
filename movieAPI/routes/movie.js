c5f3aece43f4cfd9644f62486fbba351177af851
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-28 13:48:09 +0300

    Hata Yonetimi

--- movieAPI/routes/movie.js
+++ movieAPI/routes/movie.js
@@ -14,10 +14,14 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/:movie_id', (req, res) => {
+router.get('/:movie_id', (req, res, next) => {
    const promise = Movie.findById(req.params.movie_id);
 
    promise.then((movie) => {
+
+       if(!movie)
+           next({message: 'The film was not found!!', code: 99});
+
       res.json(movie)
    }).catch((err) => {
        res.json(err);
