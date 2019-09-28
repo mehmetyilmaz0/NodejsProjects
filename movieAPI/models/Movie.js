15392f78543a0eb55daa08dbc39fd111e56821d8
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-28 14:26:44 +0300

    date alan isim degisikligi

--- movieAPI/models/Movie.js
+++ movieAPI/models/Movie.js
@@ -11,7 +11,7 @@ const MovieSchema = new Schema({
     country: String,
     year: Number,
     imdbScore: Number,
-    date: {
+    createAt: {
         type: Date,
         default: Date.now()
     }
