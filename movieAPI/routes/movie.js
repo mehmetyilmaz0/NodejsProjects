e102da7026b88a9414ffba5726f79d7cf6889b14
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-28 14:33:53 +0300

    Iki tarih arasindaki film endpoint

--- movieAPI/routes/movie.js
+++ movieAPI/routes/movie.js
@@ -14,6 +14,21 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/between/:start_year/:end_year', (req, res) => {
+    const {start_year, end_year} = req.params;
+    const promise = Movie.find(
+        {
+            year:{"$gte": parseInt(start_year), "$lte": parseInt(end_year)}
+        }
+    );
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
 router.get('/top10', (req, res, next) => {
     const promise = Movie.find({}).limit(10).sort({imdbScore: -1});
 
