ae53568cc74d839680dbf819b1d5be35317f6e0b
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-28 14:12:25 +0300

    Film Güncelleme Endpointi

--- movieAPI/routes/movie.js
+++ movieAPI/routes/movie.js
@@ -28,6 +28,20 @@ router.get('/:movie_id', (req, res, next) => {
    });
 });
 
+router.put('/:movie_id', (req, res, next) => {
+    const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body);
+
+    promise.then((movie) => {
+
+        if(!movie)
+            next({message: 'The film was not found!!', code: 99});
+
+        res.json(movie)
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
 
 router.post('/', (req, res, next) => {
   const {title, imdbScore, category, country, year} = req.body;
