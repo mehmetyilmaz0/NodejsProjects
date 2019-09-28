2bb1ca1273cf9701769c089e75b5ed192c5c4c91
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-28 19:07:42 +0300

    movie app with register api

--- movieAPIwithRegister/routes/index.js
+++ movieAPIwithRegister/routes/index.js
@@ -1,9 +1,33 @@
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
+
+//Models
+const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/register', (req, res, next) => {
+  const {username, password} = req.body;
+
+  bcrypt.hash(password, 10).then(function(hash) {
+    const user = new User({
+      username: username,
+      password: hash
+    });
+
+    const promise = user.save();
+
+    promise.then((user) => {
+      res.json(user);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+});
+
 
 module.exports = router;
