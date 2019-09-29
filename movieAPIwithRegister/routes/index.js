130786ef4662da78daa2a485b0e104e71afa5427
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-29 03:48:59 +0300

    Jsonwebtoken ile token oluşturma servisinin yazılması

--- movieAPIwithRegister/routes/index.js
+++ movieAPIwithRegister/routes/index.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 //Models
 const User = require('../models/User');
@@ -8,6 +9,49 @@ const User = require('../models/User');
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/authenticate', (req, res, next) => {
+  const {username, password} = req.body;
+
+  User.findOne({
+    username: username
+  }, (err, user) => {
+
+    if(err)
+      throw err;
+
+    if(!user){
+      res.json({
+        status: false,
+        message: 'Authentication failed, user not found '
+      });
+    } else {
+      bcrypt.compare(password, user.password).then((result) => {
+        if(!result){
+          res.json({
+            status: false,
+            message: 'Authentication failed, password is wrong '
+          });
+        } else {
+
+          const payload = {
+            username: username
+          };
+
+          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+            expiresIn: 720 // 12 saat
+          });
+
+          res.json({
+            status: true,
+            token: token
+          });
+        }
+      });
+    }
+
+  });
 });
 
 router.post('/register', (req, res, next) => {
