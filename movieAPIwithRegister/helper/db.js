5afde082e77a5b3443826086eb7c7c60e30c50a8
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-09-29 12:54:01 +0300

    Test ortamının hazırlanması ve ilk test kodu (describe ve it kavramları)

--- movieAPIwithRegister/helper/db.js
+++ movieAPIwithRegister/helper/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://admin:admin123@ds021663.mlab.com:21663/heroku_hwmj60vt', {useMongoClient : true});
+    mongoose.connect('mongodb://admin:admin123@ds021663.mlab.com:21663/heroku_hwmj60vt');
 
     mongoose.connection.on('open', () => {
        console.log('MongoDB : Connected....');
